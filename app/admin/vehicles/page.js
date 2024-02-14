dac98c68f0261267de092df806dbadf96901c443
Shaim Khanusiya <shaim456@gmail.com>
2024-02-14 14:48:17 +0530

    Admin can export parking data as csv

--- app/admin/vehicles/page.js
+++ app/admin/vehicles/page.js
@@ -1,9 +1,10 @@
 "use client";
 import { useEffect, useState } from "react";
 import Sidebar from "../../components/Sidebar";
-import { Flex, useToast } from "@chakra-ui/react";
+import { Flex, useToast, Button } from "@chakra-ui/react";
 import AdminTable from "../../components/adminTable";
 import axios from "axios";
+import { CSVLink } from "react-csv";
 
 const page = () => {
   const toast = useToast();
@@ -42,7 +43,21 @@ const page = () => {
     <Flex direction="row" w="full" h="screen">
       <Sidebar />
 
-      {tableData.length && <AdminTable data={tableData} />}
+      {tableData.length &&
+      <>
+        <CSVLink
+          data={tableData}
+          filename={"water-ville-parking.csv"}
+          className="btn btn-primary"
+          target="_blank"
+        >
+          <Button colorScheme="teal">Export Data</Button>
+        </CSVLink>;
+     
+       <AdminTable data={tableData} />
+       </>
+       }
+       
     </Flex>
   );
 };
