ac3bd689196f3c7877964b9f16c21a164e9e8d4a
Shaim Khanusiya <31386526+r00tshaim@users.noreply.github.com>
2024-02-18 13:56:49 +0530

    todays enteries api (#24)

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ import { verifyAuth, isAdmin } from "@/lib/auth";
 
 
 // This function can be marked `async` if using `await` inside
-export async function middleware (request ,response) {
+export async function middleware (request) {
   // there are some public paths and there are some protected paths
   // the public path should not be visible when the user has the token
   // the private path should not be visible when the user doesn't have the token
@@ -14,15 +14,23 @@ export async function middleware (request ,response) {
 
    // validate the user is authenticated
    const verifiedToken = await verifyAuth(request);
-  
     //validate the user is authorized
   if(verifiedToken == null) {
     console.log("token verification failed")
-    return NextResponse.redirect(new URL("/", request.nextUrl));
+    //skip token for /api/login
+    //not skipping for /api/signup, as only logged in admin can create client users
+    if(path === "/api/login") {
+      return NextResponse.next();
+    }
+    if(path.startsWith("/api")) {
+      return NextResponse.json({message: "Not authorized"}, {status: 401});
+    } else {
+      return NextResponse.redirect(new URL("/", request.nextUrl));
+    }
   }
 
   const isAdmin_ = isAdmin(verifiedToken);
-  console.log("isAdmin_ = ", isAdmin_)
+  //console.log("isAdmin_ = ", isAdmin_)
 
   //https://nextjs.org/docs/messages/returning-response-body-in-middleware
   //https://medium.com/sopra-steria-norge/how-to-write-actual-api-middleware-for-next-js-2a38355f6674
@@ -35,6 +43,21 @@ export async function middleware (request ,response) {
     //console.log("DENIED response1 = ", response)
     //return response;
     return NextResponse.redirect(new URL("/", request.nextUrl));
+  } else if(path.startsWith("/client")) {
+    if(isAdmin_) {
+      console.log("/client is blocked. Redirecting to /admin")
+      return NextResponse.redirect(new URL("/admin", request.nextUrl));
+    } else {
+      console.log("/client is blocked. Redirecting to / ")
+      return NextResponse.redirect(new URL("/", request.nextUrl));
+    } 
+  } else if(path.startsWith("/api")) {
+    if(path === "/api/login") {
+      return NextResponse.next();
+    }
+    if(!isAdmin_) {
+      return NextResponse.json({message: "Not authorized for this route"}, {status: 401});
+    }
   }
 
   /*
@@ -56,5 +79,5 @@ export async function middleware (request ,response) {
 //For our use case, we are only defining three routes that will trigger this middleware: /profile, /login, /sign-up
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/login", "/signup", "/client", "/admin", "/admin/:path*", "/client/:path*"],
+  matcher: ["/login", "/signup", "/client", "/admin", "/admin/:path*", "/client/:path*", "/api/:path*"],
 };
